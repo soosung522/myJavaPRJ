76d1b9c6140bc72991e97148dc308787e2299256
data8320-03 <soosumg522@gmail.com>
2024-03-11 15:06:37 +0900

    소스 수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
 
 
-   public static void main(String[] args){
-    System.out.println("Hello!! MY First Java Coding!!");
-   }
+    public static void main(String[] args) {
+        System.out.println("Hello!! MY First Java Coding!!");
+
+        System.out.println("Edit Java Code!!");
+    }
 }
